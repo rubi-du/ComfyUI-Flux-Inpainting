51ed47d16dbe728c5d5e8486ed5cca120ce6d5ab
duhaifeng <duhaifeng1@126.com>
2024-11-27 17:23:01 +0800

    feat: use the latest autoencoder_kl

--- modules/pipeline_flux_fill.py
+++ modules/pipeline_flux_fill.py
@@ -23,8 +23,8 @@ from transformers import CLIPTextModel, CLIPTokenizer, T5EncoderModel, T5Tokeniz
 
 from .image_processor import VaeImageProcessor
 from diffusers.loaders import FluxLoraLoaderMixin, FromSingleFileMixin, TextualInversionLoaderMixin
-from diffusers.models.autoencoders import AutoencoderKL
-from diffusers.models.transformers import FluxTransformer2DModel
+from .autoencoder_kl import AutoencoderKL
+from .transformer_override import FluxTransformer2DModel
 from diffusers.schedulers import FlowMatchEulerDiscreteScheduler
 from diffusers.utils import (
     USE_PEFT_BACKEND,
