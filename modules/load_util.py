cc0df06189163800939c80724268073212c85759
duhaifeng <duhaifeng1@126.com>
2024-11-27 17:36:57 +0800

    feat: use the latest autoencoder_kl

--- modules/load_util.py
+++ modules/load_util.py
@@ -1,6 +1,9 @@
+import os
 import torch
 from diffusers import DiffusionPipeline
 from transformers import T5EncoderModel
+
+from .autoencoder_kl import AutoencoderKL
 
 from .transformer_override import FluxTransformer2DModel
 
@@ -12,7 +15,8 @@ def load_flux_fill_nf4(flux_dir: str, flux_nf4_dir: str, four_bit=False):
     """ load flux fill nf4 """
     exclude_sub_model = ["transformer", "text_encoder_2"] if four_bit else ["transformer"]
     exclude_sub_model_dict = {model: None for model in exclude_sub_model}
-    orig_pipeline = FluxFillPipeline.from_pretrained(flux_dir, torch_dtype=dtype, **exclude_sub_model_dict)
+    vae = AutoencoderKL.from_pretrained(os.path.join(flux_dir, "vae"), torch_dtype=dtype)
+    orig_pipeline = FluxFillPipeline.from_pretrained(flux_dir, vae=vae, torch_dtype=dtype, **exclude_sub_model_dict)
     
     if four_bit:
         print("Using four bit.")
